cf9e2a80e1b49e01c894b3a8886bb64578b288c5
Jon Lake <jlake21@ford.com>
2022-03-25 10:38:33 -0600

    Add function getTransactionsByItemDescription

--- exercise.js
+++ exercise.js
@@ -3,9 +3,31 @@ function getItemById(items, id){
     return items.find(item => item.id === id)
 }
 
+// function buildTransactions(items, sales){
+//     sales.map(sale => {
+//         const item = getItemById(items, sale.itemId)
+
+//         return Object.assign(...sale, description: item.description, price: item.price)
+
+//     })
+// }
+
+// function buildTransactions(sales, items){
+//     return sales.map(sale => {
+//         return Object.assign(sale, getItemById(items, sale.itemId));
+//     }).map(sale => {
+//         delete sale.id;
+//         return sale;
+//     });
+// }
+
+function getTransactionsByItemDescription(transactions, itemDescription){
+    return transactions.filter(transaction => transaction.description === itemDescription);
+}
+
 module.exports = {
     // Uncomment these as you write them
-     getItemById,
+    getItemById,
     // buildTransactions,
-    // getTransactionsByItemDescription
+    getTransactionsByItemDescription
 }
